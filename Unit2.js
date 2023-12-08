88d5b8c7975e3f4891d2b0a51ee17420720794cb
Sabau David <148950120+davidmnb@users.noreply.github.com>
2023-12-08 17:08:28 +0200

    Add files via upload

---
+++ Unit2.js
@@ -0,0 +1,27 @@
+//cache the DOM 
+document.addEventListener('DOMContentLoaded',function(){
+	var slideshowbutton= document.GetElementById('slideshowbutton')
+	var img1= document.GetElementById('img1')
+	var img2= document.GetElementById('img2')
+	var isGradientSet=false;
+	var maintitle=documentGetElementById('maintitle')
+button.addEventListener('click',Slideshow)
+
+function Slideshow(){
+	if(isGradientSet){
+		maintitle.style.BackgroundImage='';
+        		
+	}else{
+		
+		
+		
+	}
+	
+	
+}	
+
+
+
+	
+});
+
